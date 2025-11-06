c6af3a7935948bf92cdaace9eaf751a13f436dad
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-06 20:33:18 +0100

    Update casts example

--- src/examples/casts/main.kt
+++ src/examples/casts/main.kt
@@ -1,24 +1,38 @@
-// Kotlin supports two types of casts.
+// Kotlin supports different types of casts.
 
 fun withCast(x: Any) {
     // This is a normal cast using the `as` operator, which will
     // throw an exception when `x` is not of the `String` type.
-    val xString = x as String
-    println("x has length ${x.length}")
+    val xstr = x as String
+    println("x has length ${xstr.length}")
 }
 
-fun withSmartCast(x: Any) {
+fun withSafeCast(y: Any) {
+    // This is a *safe cast*, using the `as?` operator. This will
+    // return `null` when `y` is not of the `String` type.
+    val ystr = y as? String
+
+    if (ystr != null) {
+        println("y has length ${ystr.length}")
+    }
+}
+
+fun withSmartCast(z: Any) {
     // Kotlin also has a feature called *smart casts*,
     // which use the `is` operator.
-    if (x is String) {
+    if (z is String) {
         // Inside this `if` block, the compiler has
-        // determined that `x` has type `String`.
-        println("x is a string with length ${x.length}")
+        // determined that `z` has type `String`.
+        println("z is a string with length ${z.length}")
     }
 
     // Smart casts also work when the `is` operator is negated.
-    if (x !is String) return
-    println("Yes, x is a string with length ${x.length}")
+    if (z !is String) {
+        println("z is not a string")
+        return
+    }
+
+    println("Yes, z is a string with length ${z.length}")
 }
 
 fun main() {
@@ -27,6 +41,9 @@ fun main() {
 
     withSmartCast(x1)
     withSmartCast(x2)
+
+    withSafeCast(x1)
+    withSafeCast(x2)
 
     withCast(x1)
     withCast(x2)
--- src/examples/casts/run.sh
+++ src/examples/casts/run.sh
@@ -1,12 +1,14 @@
 $ kotlinc main.kt -include-runtime -d main.jar
 $ java -jar Main.jar
-x is a string with length 12
-Yes, x is a string with length 12
+z is a string with length 12
+Yes, z is a string with length 12
+z is not a string
+y has length 12
 x has length 12
 Exception in thread "main" java.lang.ClassCastException:
-class java.lang.Double cannot be cast to class java.lang
-.String (java.lang.Double and java.lang.String are in mo
-dule java.base of loader 'bootstrap')
+class java.lang.Double cannot be cast to class java.lang.
+String (java.lang.Double and java.lang.String are in modu
+le java.base of loader 'bootstrap')
         at MainKt.withCast(main.kt:6)
-        at MainKt.main(main.kt:32)
+        at MainKt.main(main.kt:49)
         at MainKt.main(main.kt)
