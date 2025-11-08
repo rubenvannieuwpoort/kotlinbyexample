6586af2001547217395e2beb330b34e37e3aba16
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-08 13:21:44 +0100

    Update example for multiple return values

--- src/examples/multiple_return_values/main.kt
+++ src/examples/multiple_return_values/main.kt
@@ -27,6 +27,11 @@ fun evenMoreNames(): Quadruple<String, String,
     return Quadruple("Fernando", "de", "los", "Santos");
 }
 
+// Yet another solution is to return a list.
+fun ridiculouslyLongName(): List<String> {
+    return listOf("Adolph", "Blaine", "Charles", "David")
+}
+
 fun main() {
     val names = firstAndLastName()
     println(names.first + " " + names.second)
@@ -41,4 +46,33 @@ fun main() {
             manyNames.second + " " +
             manyNames.third + " " +
             manyNames.fourth)
+
+    // It is also possible to do a *destructuring declaration*, which
+    // "destructures" the values into separate variables.
+    val (firstName, secondName, thirdName) = allMyNames()
+    println(firstName)
+    println(secondName)
+    println(thirdName)
+
+    // Destructuring declarations also work for functions that return lists.
+    val (name1, name2) = ridiculouslyLongName()
+    println(name1)
+    println(name2)
+
+    // Destructuring declarations are just syntactic sugar. This is the
+    // desugared version of the last paragraph of code.
+    val temp = ridiculouslyLongName()
+    val desugaredName1 = temp.component1()
+    val desugaredName2 = temp.component2()
+    println(name1)
+    println(name2)
+
+    // There can be up to 5 variables in a destructuring declaration. The
+    // number of variables can not be bigger than the number of values that the
+    // function returns.
+    
+    // For types with a fixed number of values the number of values is checked
+    // at compile time. For collection types, an `IndexOutOfBoundException`
+    // can occur if the number of values in the declaration is too large.
+    val (n1, n2, n3, n4, n5) = ridiculouslyLongName()
 }
--- src/examples/multiple_return_values/run.sh
+++ src/examples/multiple_return_values/run.sh
@@ -3,3 +3,16 @@ $ java -jar Main.jar
 Ruben van Nieuwpoort
 Ruben van Nieuwpoort
 Fernando de los Santos
+Ruben
+van
+Nieuwpoort
+Adolph
+Blaine
+Adolph
+Blaine
+Exception in thread "main" java.lang.ArrayIndexOutOfBounds
+Exception: Index 4 out of bounds for length 4
+        at java.base/java.util.Arrays$ArrayList.get(Arrays
+.java:4225)
+        at MainKt.main(main.kt:77)
+        at MainKt.main(main.kt)
